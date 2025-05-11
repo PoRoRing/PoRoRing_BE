e8d393a3f52bb040574684fb78401d1bc7d354d3
Sojeong Lee <162033491+Sojeong0430@users.noreply.github.com>
2025-05-12 01:03:58 +0900

    🎉 initial commit

---
+++ src/main/java/Server/Pororing/PororingApplication.java
@@ -0,0 +1,13 @@
+package Server.Pororing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PororingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PororingApplication.class, args);
+	}
+
+}

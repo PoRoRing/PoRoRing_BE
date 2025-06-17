a3f3c7f24122761aa07a2e438d28f5684598891a
Sojeong Lee <162033491+Sojeong0430@users.noreply.github.com>
2025-06-17 21:20:23 +0900

    ✨[Feat] Config 설정

---
+++ src/main/java/Server/Pororing/config/RestTemplateConfig.java
@@ -0,0 +1,13 @@
+package Server.Pororing.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class RestTemplateConfig {
+    @Bean
+    public RestTemplate geminiRestTemplate(){
+        return new RestTemplate();
+    }
+}
